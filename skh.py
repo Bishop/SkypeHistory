c3e3df858c9e45cc977d9a34a709a84d4b1185d0
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-05-15 01:40:33 +0300

    Create intermediate database

--- skh.py
+++ skh.py
@@ -1,3 +1,4 @@
+import sqlite3
 import sys
 import options
 
@@ -6,3 +7,19 @@ VERSION = "0.0.1"
 options = options.get_options(VERSION)
 
 print options
+
+def check_export_table(conn):
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS message (date INTEGER, message TEXT, author TEXT)
+    """)
+
+    conn.commit()
+
+conn = sqlite3.connect('skh.db')
+
+check_export_table(conn)
+
+
+conn.close()
