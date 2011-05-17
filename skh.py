1e109b2cffce697dcccd4478db9dacbffe7f990f
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-05-17 10:57:45 +0300

    Simple export database structure

--- skh.py
+++ skh.py
@@ -9,13 +9,32 @@ options = options.get_options(VERSION)
 print options
 
 def check_export_table(conn):
-    cursor = conn.cursor()
+    c = conn.cursor()
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS message (date INTEGER, message TEXT, author TEXT)
-    """)
+    sqls = [
+        """
+            CREATE TABLE IF NOT EXISTS main.message (
+                id INTEGER NOT NULL PRIMARY KEY,
+                timestamp INTEGER NOT NULL,
+                adder TEXT NOT NULL,
+                message TEXT,
+                UNIQUE (timestamp, adder) ON CONFLICT IGNORE
+            )
+        """,
+        """
+            CREATE TABLE IF NOT EXISTS main.contact (
+                id INTEGER NOT NULL PRIMARY KEY,
+                skypename TEXT,
+                fullname TEXT,
+                birthday INTEGER,
+                gender INTEGER,
+                UNIQUE (skypename) ON CONFLICT IGNORE
+            )
+        """
+    ]
 
-    conn.commit()
+    for sql in sqls:
+        c.execute(sql)
 
 conn = sqlite3.connect('skh.db')
 
